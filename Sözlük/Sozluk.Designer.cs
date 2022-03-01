508a68169b4a14cb101ea3235a5ad548df643ec1
Mali <nlu@outlook.com.tr>
2022-03-01 22:11:25 +0300

    Add project files.

---
+++ Sözlük/Sozluk.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Sözlük
+{
+    partial class Sozluk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textGirilenKelime = new System.Windows.Forms.TextBox();
+            this.buttonKelimeBul = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kelime giriniz:";
+            // 
+            // textGirilenKelime
+            // 
+            this.textGirilenKelime.Location = new System.Drawing.Point(162, 22);
+            this.textGirilenKelime.Name = "textGirilenKelime";
+            this.textGirilenKelime.Size = new System.Drawing.Size(241, 31);
+            this.textGirilenKelime.TabIndex = 1;
+            this.textGirilenKelime.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textGirilenKelime_KeyDown);
+            // 
+            // buttonKelimeBul
+            // 
+            this.buttonKelimeBul.Location = new System.Drawing.Point(437, 22);
+            this.buttonKelimeBul.Name = "buttonKelimeBul";
+            this.buttonKelimeBul.Size = new System.Drawing.Size(111, 31);
+            this.buttonKelimeBul.TabIndex = 3;
+            this.buttonKelimeBul.Text = "Bul";
+            this.buttonKelimeBul.UseVisualStyleBackColor = true;
+            this.buttonKelimeBul.Click += new System.EventHandler(this.buttonKelimeBul_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(516, 147);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Sozluk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 234);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonKelimeBul);
+            this.Controls.Add(this.textGirilenKelime);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Outfit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Sozluk";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sözlük";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textGirilenKelime;
+        private System.Windows.Forms.Button buttonKelimeBul;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Sözlük/Sozluk.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Globalization;
+
+namespace Sözlük
+{
+    public partial class Sozluk : Form
+    {
+        public Sozluk()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKelimeBul_Click_1(object sender, EventArgs e)
+        {
+            // Kelimeyi aramadan önce internet bağlantısını kontrol ediyor.
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                // sozluk.gov.tr apisini kullanarak girilen kelimenin anlamını buluyor.
+                string adres = "https://sozluk.gov.tr/gts?ara=" + textGirilenKelime.Text.ToLower();
+                WebRequest istek = HttpWebRequest.Create(adres);
+                WebResponse cevap;
+                cevap = istek.GetResponse();
+                StreamReader donenbilgiler = new StreamReader(cevap.GetResponseStream());
+                string gelen = donenbilgiler.ReadToEnd();
+                int kelimeanlam = gelen.IndexOf("\"anlam\"") + 9;
+                int kelimeanlambitis = gelen.Substring(kelimeanlam).IndexOf("\"");
+                string anlam = gelen.Substring(kelimeanlam, kelimeanlambitis);
+
+                string girilenkelimekucuk = textGirilenKelime.Text.ToLower();
+
+                // Girilen kelime boş ise arama yapmıyor.
+                if (!string.IsNullOrEmpty(textGirilenKelime.Text))
+                {
+                    // Eğer kelime bulunmadıysa kelimenin bulunamadığını söylüyor.
+                    if (anlam == ":")
+                    {
+                        richTextBox1.Text = "Kelime bulunamadı (" + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(girilenkelimekucuk) + ")";
+                        textGirilenKelime.Text = "";
+                        textGirilenKelime.Focus();
+                    }
+                    else
+                    {
+                        richTextBox1.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(girilenkelimekucuk) + ": " + anlam;
+
+                        //Girilen kelimeyi renkli yapıyor.
+                        int kelimeuzunluk = textGirilenKelime.Text.Length;
+                        richTextBox1.SelectionStart = 0;
+                        richTextBox1.SelectionLength = kelimeuzunluk + 1;
+                        richTextBox1.SelectionColor = Color.Green;
+                        richTextBox1.SelectionStart = this.richTextBox1.TextLength;
+                        textGirilenKelime.Text = "";
+                        textGirilenKelime.Focus();
+                    }
+                }
+            }
+            else
+                MessageBox.Show("İnternet bağlantınızı kontrol edin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void textGirilenKelime_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Girilen kelimeyi 'enter' tuşuna basıldığında aramasını sağlıyor.
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                buttonKelimeBul.PerformClick();
+            }
+        }
+    }
+}
